0d6ff2546280a1d8743729225d78a8b785266b23
GilbertPan <panzhen@inspur.com>
2020-04-11 12:55:23 +0800

    init nacos configuration

--- tangye-angel-mysql/src/main/java/com/angel/mysql/MysqlApplication.java
+++ tangye-angel-mysql/src/main/java/com/angel/mysql/MysqlApplication.java
@@ -3,10 +3,12 @@ package com.angel.mysql;
 import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceAutoConfigure;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 
 @ComponentScan("com.angel")
 @SpringBootApplication(exclude = DruidDataSourceAutoConfigure.class)
+@EnableDiscoveryClient
 public class MysqlApplication {
     public static void main(String[] args) {
         SpringApplication.run(MysqlApplication.class, args);
